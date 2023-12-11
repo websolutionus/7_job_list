683d4310515cf2dd15377e73681db7632cfde936
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-12-11 16:41:13 +0600

    Multi Auth - Creating Separate Route File And Controllers

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,6 +37,8 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+            Route::middleware('web')
+            ->group(base_path('routes/admin.php'));
         });
     }
 }
