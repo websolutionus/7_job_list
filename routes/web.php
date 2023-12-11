7a386916a8436eff4127ead4e711735bad264b7d
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-12-11 13:12:27 +0600

    Multi Auth - Working With Company and Candidate Authentication (Part - 4)

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified', 'user.role:candidate'])->name('dashboard');
-
-
-Route::get('company/dashboard', function () {
-    return view('frontend.company-dashboard.dashboard');
-})->middleware(['auth', 'verified', 'user.role:company'])->name('company.dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -35,3 +26,41 @@ Route::middleware('auth')->group(function () {
 
 
 require __DIR__.'/auth.php';
+
+
+/** Candidate Dashboard Routes */
+Route::group(
+    [
+        'middleware' => ['auth', 'verified', 'user.role:candidate'],
+        'prefix' => 'candidate',
+        'as' => 'candidate.'
+    ],
+    function() {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+});
+
+/** Company Dashboard Routes */
+Route::group(
+    [
+        'middleware' => ['auth', 'verified', 'user.role:company'],
+        'prefix' => 'company',
+        'as' => 'company.'
+    ],
+    function() {
+
+    Route::get('/dashboard', function () {
+        return view('frontend.company-dashboard.dashboard');
+    })->name('dashboard');
+
+});
+
+
+
+
+
+
+
