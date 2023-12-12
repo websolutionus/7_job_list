fef1236bb211aef7e36684e63f315a7d968ec24d
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-12-12 11:08:48 +0600

    Multi Auth - Working with Admin Logout Feature

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -39,10 +39,10 @@
                         </x-dropdown-link>
 
                         <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
+                        <form method="POST" action="{{ auth()->guard('admin')->check() ? route('admin.logout') : route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')"
+                            <x-dropdown-link :href="auth()->guard('admin')->check() ? route('admin.logout') : route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
