44a2f7e0e6a8675330b3d75fb68706d53ed7a893
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-12-13 09:38:23 +0600

    Admin Panel Setup - Removing Unnecessary Scripts and Css Tags From Master

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -10,9 +10,6 @@
   <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
 
   <!-- CSS Libraries -->
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/jqvmap/dist/jqvmap.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/weather-icon/css/weather-icons.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/weather-icon/css/weather-icons-wind.min.css') }}">
   <link rel="stylesheet" href="{{ asset('admin/assets/modules/summernote/summernote-bs4.css') }}">
 
   <!-- Template CSS -->
@@ -53,15 +50,8 @@
   <script src="{{ asset('admin/assets/js/stisla.js') }}"></script>
 
   <!-- JS Libraies -->
-  <script src="{{ asset('admin/assets/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/chart.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/summernote/summernote-bs4.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
 
-  <!-- Page Specific JS File -->
-  <script src="{{ asset('admin/assets/js/page/index-0.js') }}"></script>
+  <script src="{{ asset('admin/assets/modules/summernote/summernote-bs4.js') }}"></script>
 
   <!-- Template JS File -->
   <script src="{{ asset('admin/assets/js/scripts.js') }}"></script>
