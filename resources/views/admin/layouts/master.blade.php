8e9f6d84b862159780c87af5d9c152137761dad2
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-12-19 12:59:06 +0600

    Industry Types - Working with Delete Feature (Part - 1)

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,64 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>General Dashboard &mdash; Stisla</title>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>General Dashboard &mdash; Stisla</title>
 
-  @notifyCss
-  <!-- General CSS Files -->
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/bootstrap/css/bootstrap.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
+    @notifyCss
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/modules/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
 
-  <!-- CSS Libraries -->
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/summernote/summernote-bs4.css') }}">
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/modules/summernote/summernote-bs4.css') }}">
 
-  <!-- Template CSS -->
-  <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
-  <link rel="stylesheet" href="{{ asset('admin/assets/css/components.css') }}">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/components.css') }}">
 </head>
 
 <body>
-  <div id="app">
-    <div class="main-wrapper main-wrapper-1">
-      <div class="navbar-bg"></div>
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="navbar-bg"></div>
 
-      @include('admin.layouts.sidebar')
+            @include('admin.layouts.sidebar')
 
-      <!-- Main Content -->
-      <div class="main-content">
-        @yield('contents')
-      </div>
+            <!-- Main Content -->
+            <div class="main-content">
+                @yield('contents')
+            </div>
 
-      <footer class="main-footer">
-        <div class="footer-left">
-          Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Design By <a href="www.websolutionus.com">websolutionus</a>
+            <footer class="main-footer">
+                <div class="footer-left">
+                    Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Design By <a
+                        href="www.websolutionus.com">websolutionus</a>
+                </div>
+                <div class="footer-right">
+
+                </div>
+            </footer>
         </div>
-        <div class="footer-right">
-
-        </div>
-      </footer>
     </div>
-  </div>
 
-  <!-- General JS Scripts -->
-  <script src="{{ asset('admin/assets/modules/jquery.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/popper.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/tooltip.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/moment.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/js/stisla.js') }}"></script>
+    <!-- General JS Scripts -->
+    <script src="{{ asset('admin/assets/modules/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/popper.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/tooltip.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/moment.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/stisla.js') }}"></script>
 
-  <!-- JS Libraies -->
-  <script src="{{ asset('admin/assets/modules/summernote/summernote-bs4.js') }}"></script>
+    <!-- JS Libraies -->
+    <script src="{{ asset('admin/assets/modules/summernote/summernote-bs4.js') }}"></script>
+    <script src="{{ asset('admin/assets/modules/sweetalert/sweetalert.min.js') }}"></script>
     <!-- laravel notify start-->
     <x-notify::notify />
     <!-- laravel notify end-->
     @notifyJs
 
-  <!-- Template JS File -->
-  <script src="{{ asset('admin/assets/js/scripts.js') }}"></script>
-  <script src="{{ asset('admin/assets/js/custom.js') }}"></script>
+    <!-- Template JS File -->
+    <script src="{{ asset('admin/assets/js/scripts.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/custom.js') }}"></script>
+
+    <script>
+        $(".delete-item").on('click', function(e) {
+            e.preventDefault();
+
+            swal({
+                    title: 'Are you sure?',
+                    text: 'Once deleted, you will not be able to recover this data!',
+                    icon: 'warning',
+                    buttons: true,
+                    dangerMode: true,
+                })
+                .then((willDelete) => {
+                    if (willDelete) {
+                        swal('Poof! Your imaginary file has been deleted!', {
+                            icon: 'success',
+                        });
+                    } else {
+                        swal('Your imaginary file is safe!');
+                    }
+                });
+        });
+    </script>
 </body>
+
 </html>
