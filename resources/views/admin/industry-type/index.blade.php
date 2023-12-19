88b7a7eb568afe1c816ab5a4dc79e36a870b31c6
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-12-19 09:33:19 +0600

    Industry Types - Creating Necessary Files and Designs ( Part - 2)

--- resources/views/admin/industry-type/index.blade.php
+++ resources/views/admin/industry-type/index.blade.php
@@ -1,5 +1,41 @@
 @extends('admin.layouts.master')
 
 @section('contents')
-hello world
+    <section class="section">
+        <div class="section-header">
+            <h1>Industry Type</h1>
+        </div>
+
+        <div class="section-body">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>All Industry Types</h4>
+                        <div class="card-header-form">
+                            <form>
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                    <div class="input-group-btn">
+                                        <button style="height: 40px;" class="btn btn-primary"><i class="fas fa-search"></i></button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <a href="" class="btn btn-primary"> <i class="fas fa-plus-circle"></i> Create new</a>
+                    </div>
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <tr>
+
+                                </tr>
+
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
