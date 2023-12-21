16246b122b341b6ecad28a59a2e5097d49870c70
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-12-21 13:47:51 +0600

    Admin Sidebar - Working with Admin Sidebar Activation

--- app/Helpers/GlobalHelper.php
+++ app/Helpers/GlobalHelper.php
@@ -7,3 +7,17 @@ if(!function_exists('hasError')) {
         return $errors->has($name) ? 'is-invalid' : '';
     }
 }
+
+/** Set sidebar active */
+
+if(!function_exists('setSidebarActive')) {
+    function setSidebarActive(array $routes) : ?String
+    {
+        foreach($routes as $route) {
+            if(request()->routeIs($route)) {
+                return 'active';
+            }
+        }
+        return null;
+    }
+}
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -100,30 +100,30 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="active">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+            <li class="{{ setSidebarActive(['admin.dashboard']) }}">
+                <a href="{{ route('admin.dashboard') }}" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
 
             </li>
             <li class="menu-header">Starter</li>
 
-            <li class="dropdown">
+            <li class="dropdown {{ setSidebarActive(['admin.industry-types.*', 'admin.organization-types.*']) }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Attributes</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="{{ route('admin.industry-types.index') }}">Industry Type</a></li>
-                    <li><a class="nav-link" href="{{ route('admin.organization-types.index') }}">Orginization Type</a></li>
+                    <li class="{{ setSidebarActive(['admin.industry-types.*']) }}"><a class="nav-link" href="{{ route('admin.industry-types.index') }}">Industry Type</a></li>
+                    <li class="{{ setSidebarActive(['admin.organization-types.*']) }}"><a class="nav-link" href="{{ route('admin.organization-types.index') }}">Orginization Type</a></li>
 
                 </ul>
             </li>
 
 
-            <li class="dropdown">
+            <li class="dropdown {{ setSidebarActive(['admin.countries.*', 'admin.states.*', 'admin.cities.*']) }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Locations</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="{{ route('admin.countries.index') }}">Countries</a></li>
-                    <li><a class="nav-link" href="{{ route('admin.states.index') }}">States</a></li>
-                    <li><a class="nav-link" href="{{ route('admin.cities.index') }}">Cities</a></li>
+                    <li class="{{ setSidebarActive(['admin.countries.*']) }}"><a class="nav-link" href="{{ route('admin.countries.index') }}">Countries</a></li>
+                    <li class="{{ setSidebarActive(['admin.states.*']) }}"><a class="nav-link" href="{{ route('admin.states.index') }}">States</a></li>
+                    <li class="{{ setSidebarActive(['admin.cities.*']) }}"><a class="nav-link" href="{{ route('admin.cities.index') }}">Cities</a></li>
 
 
                 </ul>
