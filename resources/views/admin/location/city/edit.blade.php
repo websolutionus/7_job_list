56cdf4311a63d3ecbaa26efa46a11979a50fdea7
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-12-21 12:48:23 +0600

    City- Working with Delete Feature

--- resources/views/admin/location/city/edit.blade.php
+++ resources/views/admin/location/city/edit.blade.php
@@ -3,38 +3,50 @@
 @section('contents')
     <section class="section">
         <div class="section-header">
-            <h1>States</h1>
+            <h1>Cities</h1>
         </div>
 
         <div class="section-body">
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4>Update State</h4>
+                        <h4>Update City</h4>
 
                     </div>
                     <div class="card-body">
-                        <form action="{{ route('admin.states.update', $state->id) }}" method="POST">
+                        <form action="{{ route('admin.cities.update', $city->id) }}" method="POST">
                             @csrf
                             @method('PUT')
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="">Country</label>
-                                        <select name="country" id="" class="form-control select2 {{ hasError($errors, 'country') }}">
+                                        <select name="country" id="" class="form-control select2 country {{ hasError($errors, 'country') }}">
                                             <option value="">Select</option>
                                             @foreach ($countries as $country)
-                                                <option @selected($state->country_id === $country->id) value="{{ $country->id }}">{{ $country->name }}</option>
+                                                <option @selected($country->id === $city->country_id) value="{{ $country->id }}">{{ $country->name }}</option>
                                             @endforeach
                                         </select>
                                         <x-input-error :messages="$errors->get('country')" class="mt-2" />
                                     </div>
                                 </div>
-                                <div class="col-md-6">
+                                <div class="col-md-4">
                                     <div class="form-group">
-                                        <label for="">State Name</label>
-                                        <input type="text" class="form-control {{ hasError($errors, 'name') }}" name="name" value="{{ old('name', $state->name) }}">
-                                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                                        <label for="">States</label>
+                                        <select name="state" id="" class="form-control select2 state {{ hasError($errors, 'state') }}">
+                                            <option value="">Select</option>
+                                            @foreach ($states as $state)
+                                            <option @selected($state->id === $city->state_id) value="{{ $state->id }}">{{ $state->name }}</option>
+                                             @endforeach
+                                        </select>
+                                        <x-input-error :messages="$errors->get('state')" class="mt-2" />
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="">City Name</label>
+                                        <input type="text" class="form-control {{ hasError($errors, 'city') }}" name="city" value="{{ old('city', $city->name) }}">
+                                        <x-input-error :messages="$errors->get('city')" class="mt-2" />
                                     </div>
                                 </div>
                             </div>
@@ -49,3 +61,30 @@
     </section>
 
 @endsection
+
+@push('scripts')
+<script>
+    $(document).ready(function() {
+        $('.country').on('change', function() {
+            let country_id = $(this).val();
+
+            $.ajax({
+                mehtod: 'GET',
+                url: '{{ route("admin.get-states", ":id") }}'.replace(":id", country_id),
+                data: {},
+                success: function(response) {
+                    let html = '';
+
+                    $.each(response, function(index, value) {
+                        html += `<option value="${value.id}" >${value.name}</option>`
+                    });
+                    $('.state').html(html);
+                },
+                error: function(xhr, status, error) {
+
+                }
+            })
+        })
+    })
+</script>
+@endpush
