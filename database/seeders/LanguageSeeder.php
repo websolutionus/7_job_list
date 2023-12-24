41a2d013c7fded21470431740e4b8228ea4cb93a
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-12-24 17:26:52 +0600

    Language Module - Adding Seeders

---
+++ database/seeders/LanguageSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Language;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LanguageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $languages = ['English', 'Hindi', 'Arabic', 'Bangla'];
+
+        foreach($languages as $language) {
+            $lang = new Language();
+            $lang->name = $language;
+            $lang->save();
+        }
+    }
+}
