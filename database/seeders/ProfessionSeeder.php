6e57103f3437f3efd7f8971acccd2f6e3158e8f4
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-12-25 10:23:39 +0600

    Profession Module - Adding Some Seeders

--- database/seeders/ProfessionSeeder.php
+++ database/seeders/ProfessionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Profession;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,33 @@ class ProfessionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $professions = [
+            'Software Developer',
+            'Web Designer',
+            'Data Scientist',
+            'Digital Marketer',
+            'UX/UI Designer',
+            'Network Administrator',
+            'Content Creator',
+            'Project Manager',
+            'Graphic Designer',
+            'Database Administrator',
+            'Cybersecurity Analyst',
+            'Systems Analyst',
+            'Mobile App Developer',
+            'Game Developer',
+            'Technical Writer',
+            'DevOps Engineer',
+            'Cloud Architect',
+            'Business Analyst',
+            'UI/UX Developer',
+            'Artificial Intelligence Engineer',
+        ];
+
+        foreach($professions as $profession) {
+            $data = new Profession();
+            $data->name = $profession;
+            $data->save();
+        }
     }
 }
