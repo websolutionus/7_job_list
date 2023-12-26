a4dc35a787975b4da079fd95a8b59f3b31d262df
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-12-26 17:39:38 +0600

    Experience - Working with Experience Module (Part - 2)

--- resources/views/frontend/candidate-dashboard/profile/index.blade.php
+++ resources/views/frontend/candidate-dashboard/profile/index.blade.php
@@ -123,6 +123,28 @@
             </div>
         </div>
     </section>
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            ...
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Understood</button>
+            </div>
+        </div>
+        </div>
+    </div>
+
+
+
 @endsection
 
 @push('scripts')
