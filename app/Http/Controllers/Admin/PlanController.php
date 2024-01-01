5c055ac36136e4fba563bea575e11ca4abf5037c
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-01-01 16:17:41 +0600

    Pricing System  - Working With Create Feature (Part - 1)

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -19,17 +20,19 @@ class PlanController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create() : View
     {
-        //
+        return view('admin.plan.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request) : RedirectResponse
     {
-        //
+        // store data
+        dd($request->all());
+        return to_route('admin.plans.index');
     }
 
     /**
