9c5acc623c1794ca990388046cb12015a10ec30b
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-01-03 13:22:49 +0600

    General Setting -  Working with Update Form

--- app/Http/Controllers/Admin/SiteSettingController.php
+++ app/Http/Controllers/Admin/SiteSettingController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\GeneralSettingUpdateRequest;
+use App\Models\SiteSetting;
+use App\Services\Notify;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -10,5 +14,20 @@ class SiteSettingController extends Controller
 {
     function index() : View {
         return view('admin.site-setting.index');
+    }
+
+    function updateGeneralSetting(GeneralSettingUpdateRequest $request) : RedirectResponse {
+        $validatedData = $request->validated();
+
+        foreach($validatedData as $key => $value) {
+            SiteSetting::updateOrCreate(
+                ['key' => $key],
+                ['value' => $value]
+            );
+        }
+
+        Notify::updatedNotification();
+
+        return redirect()->back();
     }
 }
--- app/Models/SiteSetting.php
+++ app/Models/SiteSetting.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class SiteSetting extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['key', 'value'];
 }
