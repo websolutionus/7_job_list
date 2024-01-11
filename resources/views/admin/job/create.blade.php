36a00543908b2a9ab4bc9a7c0b2aad81b7f45ae5
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-01-11 10:06:27 +0600

    Job Posting - Working With Create Form (Part - 2)

--- resources/views/admin/job/create.blade.php
+++ resources/views/admin/job/create.blade.php
@@ -65,6 +65,99 @@
                             </div>
                         </div>
 
+                        <div class="card">
+                            <div class="card-header">
+                                Location
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="">Country </label>
+                                            <select name="country" id="" class="form-control select2 {{ hasError($errors, 'country') }}" >
+                                                <option value="">Choose</option>
+                                            </select>
+                                            <x-input-error :messages="$errors->get('country')" class="mt-2" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="">State </label>
+                                            <select name="state" id="" class="form-control select2 {{ hasError($errors, 'state') }}" >
+                                                <option value="">Choose</option>
+                                            </select>
+                                            <x-input-error :messages="$errors->get('state')" class="mt-2" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="">City </label>
+                                            <select name="city" id="" class="form-control select2 {{ hasError($errors, 'city') }}" >
+                                                <option value="">Choose</option>
+                                            </select>
+                                            <x-input-error :messages="$errors->get('city')" class="mt-2" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="">Address <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control {{ hasError($errors, 'address') }}"
+                                                name="address" value="{{ old('address') }}">
+                                            <x-input-error :messages="$errors->get('address')" class="mt-2" />
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card">
+                            <div class="card-header">
+                                Salary Details
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-2">
+                                        <div class="form-group">
+                                            <input type="radio" id="salary_range" class="from-control {{ hasError($errors, 'salary_mode') }}" name="salary_mode" checked>
+                                            <label for="salary_range">Salary Range </label>
+                                            <x-input-error :messages="$errors->get('salary_mode')" class="mt-2" />
+                                        </div>
+                                    </div>
+                                    <div class="col-2">
+                                        <div class="form-group">
+                                            <input type="radio" id="custom_salary" class="from-control {{ hasError($errors, 'salary_mode') }}" name="salary_mode" >
+                                            <label for="custom_salary">Custom Range </label>
+                                            <x-input-error :messages="$errors->get('salary_mode')" class="mt-2" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="">Country </label>
+                                            <select name="country" id="" class="form-control select2 {{ hasError($errors, 'country') }}" >
+                                                <option value="">Choose</option>
+                                            </select>
+                                            <x-input-error :messages="$errors->get('country')" class="mt-2" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="">Address <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control {{ hasError($errors, 'address') }}"
+                                                name="address" value="{{ old('address') }}">
+                                            <x-input-error :messages="$errors->get('address')" class="mt-2" />
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                            </div>
+                        </div>
+
 
                         <div class="form-group">
                             <button type="submit" class="btn btn-primary">Create</button>
