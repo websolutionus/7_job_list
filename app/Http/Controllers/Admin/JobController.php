130a273b7ed30a2bb5e74725d0f92b4d02f8a6a4
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-01-15 09:04:47 +0600

    Job Posting - Working With Update Feature (Part -1)

--- app/Http/Controllers/Admin/JobController.php
+++ app/Http/Controllers/Admin/JobController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\JobCreateRequest;
 use App\Models\Benefits;
+use App\Models\City;
 use App\Models\Company;
 use App\Models\Country;
 use App\Models\Education;
@@ -18,6 +19,7 @@ use App\Models\JobTag;
 use App\Models\JobType;
 use App\Models\SalaryType;
 use App\Models\Skill;
+use App\Models\State;
 use App\Models\Tag;
 use App\Services\Notify;
 use App\Traits\Searchable;
@@ -151,7 +153,34 @@ class JobController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $job = Job::findOrFail($id);
+        $companies = Company::where(['profile_completion' => 1, 'visibility' => 1])->get();
+        $categories = JobCategory::all();
+        $countries = Country::all();
+        $states = State::where('country_id', $job->country_id)->get();
+        $cities = City::where('state_id', $job->state_id)->get();
+        $salaryTypes = SalaryType::all();
+        $experiences = Experience::all();
+        $jobRoles = JobRole::all();
+        $educations = Education::all();
+        $jobTypes = JobType::all();
+        $tags = Tag::all();
+        $skills = Skill::all();
+        return view('admin.job.edit', compact(
+            'companies',
+            'categories',
+            'countries',
+            'states',
+            'cities',
+            'salaryTypes',
+            'experiences',
+            'jobRoles',
+            'educations',
+            'jobTypes',
+            'tags',
+            'skills',
+            'job'
+        ));
     }
 
     /**
