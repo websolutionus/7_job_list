469235d6310db05148b4955a46f1ef6048d75ca4
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-01-25 16:54:16 +0600

    Why Choose us - Working with Why Choose us Section (Part - 3)

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -8,6 +8,7 @@ use App\Models\Hero;
 use App\Models\Job;
 use App\Models\JobCategory;
 use App\Models\Plan;
+use App\Models\WhyChooseUs;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -24,7 +25,8 @@ class HomeController extends Controller
         }])->where('show_at_popular', 1)->get();
         $featuredCategories = JobCategory::where('show_at_featured', 1)->take(10)->get();
         $jobCount = Job::count();
+        $whyChooseUs = WhyChooseUs::first();
         $plans = Plan::where(['frontend_show' => 1, 'show_at_home' => 1])->get();
-        return view('frontend.home.index', compact('plans', 'hero', 'jobCategories', 'countries', 'jobCount', 'popularJobCategories', 'featuredCategories'));
+        return view('frontend.home.index', compact('plans', 'hero', 'jobCategories', 'countries', 'jobCount', 'popularJobCategories', 'featuredCategories', 'whyChooseUs'));
     }
 }
