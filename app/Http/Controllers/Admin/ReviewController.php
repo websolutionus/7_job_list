689ed9e017a718e7d39a04cfab8f9fd9bfd5977f
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-01-29 15:17:26 +0600

    Review Section - Working with Review Section (Part - 2)

--- app/Http/Controllers/Admin/ReviewController.php
+++ app/Http/Controllers/Admin/ReviewController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ReviewCreateRequest;
+use App\Models\Review;
+use App\Services\Notify;
+use App\Traits\FileUploadTrait;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class ReviewController extends Controller
 {
+    use FileUploadTrait;
     /**
      * Display a listing of the resource.
      */
@@ -19,17 +24,28 @@ class ReviewController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('admin.review.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ReviewCreateRequest $request)
     {
-        //
+        $imagePath = $this->uploadFile($request, 'image');
+
+        $review = new Review();
+        $review->image = $imagePath;
+        $review->name = $request->name;
+        $review->title = $request->title;
+        $review->review = $request->review;
+        $review->rating = $request->rating;
+        $review->save();
+
+        Notify::createdNotification();
+        return to_route('admin.reviews.index');
     }
 
     /**
---
+++ resources/views/admin/review/create.blade.php
@@ -0,0 +1,68 @@
+@extends('admin.layouts.master')
+
+@section('contents')
+    <section class="section">
+        <div class="section-header">
+            <h1>Rreviews</h1>
+        </div>
+
+        <div class="section-body">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Create Review</h4>
+
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('admin.reviews.store') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="row">
+                                <div class="col-md-5">
+                                    <div class="form-group">
+                                        {{-- <x-image-preview :height="200" :width="300" :source="$hero->image" /> --}}
+
+                                        <label for="">Image</label>
+                                        <input type="file" class="form-control {{ hasError($errors, 'image') }}" name="image">
+                                        <x-input-error :messages="$errors->get('image')" class="mt-2" />
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="form-group">
+                                <label for="">Name</label>
+                                <input type="text" class="form-control {{ hasError($errors, 'name') }}" name="name" value="{{ old('name') }}">
+                                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                            </div>
+                            <div class="form-group">
+                                <label for="">Title</label>
+                                <input type="text" class="form-control {{ hasError($errors, 'title') }}" name="title" value="{{ old('title') }}">
+                                <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="">Review</label>
+                                <textarea name="review" class="form-control {{ hasError($errors, 'review') }}"></textarea>
+                                <x-input-error :messages="$errors->get('review')" class="mt-2" />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="">Rating</label>
+                                <select name="rating"  class="form-control {{ hasError($errors, 'rating') }}">
+                                    @for ($i = 1; $i <= 5; $i++)
+                                        <option value="{{ $i }}">{{ $i }}</option>
+                                    @endfor
+                                </select>
+                                <x-input-error :messages="$errors->get('rating')" class="mt-2" />
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Create</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
