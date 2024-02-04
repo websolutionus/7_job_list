4329f3800c803798c43eeccfa6b6b08cfd74ed93
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-02-04 13:22:47 +0600

    Admin Dashboard - Working Admin Dashboard (Part -3 )

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,11 +8,14 @@ use App\Models\Candidate;
 use App\Models\Company;
 use App\Models\Job;
 use App\Models\Order;
+use App\Traits\Searchable;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
+    use Searchable;
+
     function index() : View {
         $amounts = Order::pluck('default_amount')->toArray();
         $totalEarnings = calculateEarnings($amounts);
@@ -24,9 +27,11 @@ class DashboardController extends Controller
         $pendingJobs = Job::where('status', 'pending')->count();
         $totalBlogs = Blog::count();
 
+        $query = Job::query();
+        $this->search($query, ['title', 'slug']);
+        $jobs = $query->where('status', 'pending')->orderBy('id', 'DESC')->paginate(20);
 
 
-
-        return view('admin.dashboard.index', compact('totalEarnings', 'totalCandidates', 'totalCompanies', 'totalJobs', 'activeJobs', 'expiredJobs', 'pendingJobs', 'totalBlogs'));
+        return view('admin.dashboard.index', compact('totalEarnings', 'totalCandidates', 'totalCompanies', 'totalJobs', 'activeJobs', 'expiredJobs', 'pendingJobs', 'totalBlogs', 'jobs'));
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -129,5 +129,112 @@
       </div>
     </div>
 
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4>Pending Jobs</h4>
+                <div class="card-header-form">
+                    <form action="{{ route('admin.jobs.index') }}" method="GET">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search" name="search" value="{{ request('search') }}">
+                            <div class="input-group-btn">
+                                <button type="submit" style="height: 40px;" class="btn btn-primary"><i class="fas fa-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                
+            </div>
+            <div class="card-body p-0">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Job</th>
+                            <th>Category/Role</th>
+                            <th>Salary</th>
+                            <th>Deadline</th>
+                            <th>Status</th>
+                            <th>Approve</th>
+
+                            <th style="width: 10%">Action</th>
+                        </tr>
+                    <tbody>
+                        @forelse ($jobs as $job)
+                            <tr>
+                                <td>
+                                    <div class="d-flex">
+                                        <div class="mr-2">
+                                            <img style="width:50px;height:50px;object-fit:cover" src="{{ asset($job->company->logo) }}" alt="">
+                                        </div>
+                                        <div>
+                                            <b>{{ $job->title }}</b>
+                                            <br>
+                                            <span>{{ $job->company->name }} - {{ $job->jobType->name }}</span>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div>
+                                        <b>{{ $job->category?->name }}</b>
+                                        <br>
+                                        <span>{{ $job->jobRole->name }}</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    @if ($job->salary_mode === 'range')
+                                        <b>{{ $job->min_salary }} - {{ $job->max_salary }} {{ config('settings.site_default_currency') }}</b>
+                                        <br>
+                                        <span>{{ $job->salaryType->name }}</span>
+                                    @else
+                                    <b>{{ $job->custom_salary }}</b>
+                                    <br>
+                                    <span>{{ $job->salaryType->name }}</span>
+
+                                    @endif
+                                </td>
+                                <td>{{ formatDate($job->deadline) }}</td>
+                                <td>
+                                    @if ($job->status === 'pending')
+                                    <span class="badge bg-warning text-dark">Peinding</span>
+                                    @elseif($job->deadline > date('Y-m-d'))
+                                        <span class="badge bg-primary text-dark">Active</span>
+                                    @else
+                                        <span class="badge bg-danger text-dark">Expired</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    <div class="form-group">
+                                        <label class="custom-switch mt-2">
+                                          <input @checked($job->status === 'active') type="checkbox" data-id="{{ $job->id }}" name="custom-switch-checkbox" class="custom-switch-input post_status">
+                                          <span class="custom-switch-indicator"></span>
+                                        </label>
+                                      </div>
+                                </td>
+                                <td>
+                                    <a href="{{ route('admin.jobs.edit', $job->id) }}" class="btn-sm btn btn-primary"><i class="fas fa-edit"></i></a>
+                                    <a href="{{ route('admin.jobs.destroy', $job->id) }}" class="btn-sm btn btn-danger delete-item"><i class="fas fa-trash-alt"></i></a>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="7" class="text-center">No result found!</td>
+                            </tr>
+                        @endforelse
+
+                    </tbody>
+
+                    </table>
+                </div>
+            </div>
+            <div class="card-footer text-right">
+                <nav class="d-inline-block">
+                    @if ($jobs->hasPages())
+                        {{ $jobs->withQueryString()->links() }}
+                    @endif
+                </nav>
+            </div>
+        </div>
+    </div>
+
   </section>
 @endsection
