c722202ab3e65964db317532163c1ea5181db383
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-02-07 12:29:41 +0600

    Adding Permission - Wrapping Features with Permissions (Part - 1)

--- app/Helpers/GlobalHelper.php
+++ app/Helpers/GlobalHelper.php
@@ -121,5 +121,21 @@ if(!function_exists('calculateEarnings')) {
     }
 }
 
+/** check permission */
+if(!function_exists('canAccess')) {
+    function canAccess(array $permissions) : bool
+    {
+        $permission = auth()->guard('admin')->user()->hasAnyPermission($permissions);
+        $superAdmin = auth()->guard('admin')->user()->hasRole('Super Admin');
+
+        if($permission || $superAdmin) {
+            return true;
+        }
+
+        return false;
+
+    }
+}
+
 
 
